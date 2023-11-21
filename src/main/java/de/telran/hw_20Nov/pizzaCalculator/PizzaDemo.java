4dc62094a472ffea947ed6ab8cd636a943e34a7e
Alex <alexandra.grigorchenko@gmail.com>
2023-11-21 18:14:40 +0100

    add PizzaCalculator to homework 20Nov.

---
+++ src/main/java/de/telran/hw_20Nov/pizzaCalculator/PizzaDemo.java
@@ -0,0 +1,24 @@
+package de.telran.hw_20Nov.pizzaCalculator;
+/*
+Вы работаете в пицерии.
+Клиент у Вас может заказать пиццу различных размеров и различных форм: квадратную, прямоугольную, круглую.
+Напишите методы для вычисления калорийности заказанной пиццы, если напримем кв.см. пиццы имеет 40 калорий.
+Попробуйте решить данную задачу через перегрузку методов.
+ */
+public class PizzaDemo {
+
+    public static double calcCalories(double width, double length) {
+        return width * length * 40;
+    }
+    public static double calcCalories(double radius) {
+        return radius * radius * 3.14 * 40;
+    }
+
+    public static void main(String[] args) {
+        double normalPizzaCal = calcCalories(20, 20);
+        double squarePizzaCal = calcCalories(10);
+
+        System.out.println("Normal pizza has " + normalPizzaCal + " calories");
+        System.out.println("Square pizza has " + squarePizzaCal + " calories");
+    }
+}
